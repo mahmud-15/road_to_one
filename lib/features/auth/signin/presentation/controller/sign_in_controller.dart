9a62d8bb3058acaa41e428b4cfb57f4514ebf7a9
aurnabbarua409 <aurnabbarua409@gmail.com>
2026-01-25 08:55:56 +0600

    updated

--- lib/features/auth/signin/presentation/controller/sign_in_controller.dart
+++ lib/features/auth/signin/presentation/controller/sign_in_controller.dart
@@ -76,21 +76,4 @@ class SignInController extends GetxController {
       update();
     }
   }
-
-  // Future<UserCredential> signInWithGoogle() async {
-  //   // Trigger the authentication flow
-  //   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-  //   // Obtain the auth details from the request
-  //   final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-
-  //   // Create a new credential
-  //   final credential = GoogleAuthProvider.credential(
-  //     accessToken: googleAuth?.accessToken,
-  //     idToken: googleAuth?.idToken,
-  //   );
-
-  //   // Once signed in, return the UserCredential
-  //   return await FirebaseAuth.instance.signInWithCredential(credential);
-  // }
 }
